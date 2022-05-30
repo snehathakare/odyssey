8a3ff1deea1934c0e80fc974ad48b16ce73094bc
Sneha <snhthakare@gmail.com>
2022-05-30 10:50:30 +0530

    Consume and render query data

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Layout } from '../components';
-import { gql } from '@apollo/client';
+import { Layout, QueryResult } from '../components';
+import { gql, useQuery } from '@apollo/client';
+import TrackCard from './../containers/track-card';
 
 /**
  * Tracks Page is the Catstronauts home page.
@@ -24,7 +25,15 @@ const TRACKS = gql`
 `;
 
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS)
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+  return <Layout grid><QueryResult grid error={error} loading={loading} data={data}>
+    {data?.tracksForHome?.map(track => (
+      <TrackCard key={track.id} track={track} />
+    ))}
+  </QueryResult>
+  </Layout>
 };
 
 export default Tracks;
